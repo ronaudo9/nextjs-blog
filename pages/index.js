31226da09dc3efadf9dda3a497cdb8d53f5971b9
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2022-11-07 17:55:03 +0900

    commit2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,9 +13,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-        Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className={styles.title}>
+      <Link href="/posts/first-post">
+        このページ
+      </Link>{' '}
+        を読もう！
+      </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}

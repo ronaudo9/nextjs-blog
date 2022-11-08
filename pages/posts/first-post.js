0677bbac4918d6fb766cb27bf130d25a23f1017b
ronaudo9 <naoyuki.kojima@rakus-partners.co.jp>
2022-11-08 11:58:08 +0900

    commit3 スタイルの適用

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,15 @@
 import Link from 'next/link';
+import Layout from '../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <h1>最初の投稿</h1>
       <h2>
         <Link href="/">
           トップページに戻る
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
